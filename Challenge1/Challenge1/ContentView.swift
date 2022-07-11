50414bf2480cfcb79acd55e0fe401f6cd546f230
Peyton Gasink <peyton@peytongasink.dev>
2022-07-10 21:43:57 -0500

    completed challenge 1 app

--- Challenge1/Challenge1/ContentView.swift
+++ Challenge1/Challenge1/ContentView.swift
@@ -7,10 +7,88 @@
 
 import SwiftUI
 
+enum Temperatures: String, CaseIterable {
+    case celsius = "Celsius",
+         fahrenheit = "Fahrenheit",
+         kelvin = "Kelvin"
+}
+
 struct ContentView: View {
+    @State private var inputUnit = Temperatures.celsius
+    @State private var inputTemp = 0.0
+    @State private var outputUnit = Temperatures.fahrenheit
+    
+    @FocusState private var inputTempFocused: Bool
+    
+    var outputTemp: Double {
+        // Don't bother converting if same unit
+        if inputUnit == outputUnit {
+            return inputTemp
+        }
+        
+        // Convert everything to Celsius as a starting point
+        var inputTempCelsius: Double
+        switch inputUnit {
+            case Temperatures.celsius:
+                inputTempCelsius = inputTemp
+            case Temperatures.fahrenheit:
+                inputTempCelsius =  (inputTemp - 32) * 5 / 9
+            case Temperatures.kelvin:
+                inputTempCelsius = inputTemp - 273.15
+        }
+        
+        // Convert from Celsius to whatever
+        switch outputUnit {
+            case .celsius:
+                return inputTempCelsius
+            case .fahrenheit:
+                return inputTempCelsius * 9 / 5 + 32
+            case .kelvin:
+                return inputTempCelsius + 273.15
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Input temperature", value: $inputTemp, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($inputTempFocused)
+                    Picker("Input unit", selection: $inputUnit) {
+                        ForEach(Temperatures.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            header: {
+                Text("Input Temperature")
+            }
+                
+                Section {
+                    Picker("Output unit", selection: $outputUnit) {
+                        ForEach(Temperatures.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    Text(outputTemp, format: .number)
+                }
+            header: {
+                Text("Output Temperature")
+            }
+            }
+            .navigationTitle("Temperature Converter")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        inputTempFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
