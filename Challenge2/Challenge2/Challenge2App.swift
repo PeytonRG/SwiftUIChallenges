14a78362f49b0b5a4ba0607cfc4486a6e4c37a6f
Peyton Gasink <peyton@peytongasink.dev>
2022-07-30 20:21:56 -0500

    stubbed challenge2

---
+++ Challenge2/Challenge2/Challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2App.swift
+//  Challenge2
+//
+//  Created by Peyton Gasink on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge2/Challenge2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge2
+//
+//  Created by Peyton Gasink on 7/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
